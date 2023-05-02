7c936722dd1b12a784dbf1e97b673e83cb1ec882
Joey Jacobson <joeyj1996@proton.me>
2023-05-01 19:10:56 -0500

    Completed 4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let elementsToRemove = [];
+    let elementsToKeep = [];
 
+    // Populate a set/array to check each value of the passed in array against
+    for (let i = 1; i < removeFromArray.arguments.length; i++) {
+        elementsToRemove.push(removeFromArray.arguments[i]);
+    }
+
+    for (let j = 0; j < array.length; j++) {
+        if (!(elementsToRemove.includes(array[j]))) {
+            elementsToKeep.push(array[j]);
+        }
+    }
+
+    return elementsToKeep;
 };
 
 // Do not edit below this line
