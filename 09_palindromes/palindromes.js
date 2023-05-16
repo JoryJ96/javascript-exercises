0110f60f2716e8a5b7e663b2cf0ced6d493b43c8
Joey Jacobson <joeyj1996@proton.me>
2023-05-15 21:11:33 -0500

    Passes most tests, for some reason failing the simple ones?

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,33 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string.replace(/\W/g, '');
 
+    let formattedString = string.split(" ");
+    let left = formattedString.slice(0, 1);
+    let right = formattedString.slice(-1, 1);
+
+    while (formattedString.length > 1) {
+        if (left != right) {
+            return false;
+        }
+        left = formattedString.slice(0, 1);
+        right = formattedString.slice(-1, 1);
+    }
+
+    return true;
 };
+
+function charCheck(char) {
+    switch (char) {
+        case "!":
+            return true;
+        case "?":
+            return true;
+        case ".":
+            return true;
+    }
+
+    return false;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
