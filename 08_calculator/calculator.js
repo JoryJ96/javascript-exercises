9be1d0dd5f0f6aef84b63ed5681977e807185491
Joey Jacobson <joeyj1996@proton.me>
2023-05-15 19:56:16 -0500

    Completed 08 - Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	if (array.length == 0) {
+    return 0;
+  }
+
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  if (array.length < 2) {
+    return 0;
+  }
 
+  let product = 1;
+  for (let i = 0; i < array.length; i++) {
+    product *= array[i];
+  }
+
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+  let power = num;
+	for (let i = 1; i < exp; i++) {
+    power = power * num;
+  }
+
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);  
 };
 
 // Do not edit below this line
